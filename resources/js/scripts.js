261ea48521eb1c164ead627de4eaad4847063669
Leonardo Aragão <lewoaragao@gmail.com>
2023-10-01 11:45:37 -0300

    complete video

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,15 +1,37 @@
 const listButtonCloseModal =
   document.getElementsByClassName("button-close-modal");
+const listButtonLoginAccount = document.getElementsByClassName(
+  "button-login-account"
+);
+const listButtonCreateAccount = document.getElementsByClassName(
+  "button-create-account"
+);
 
-for (var button of listButtonCloseModal) {
-  button.addEventListener("click", function (e) {
-    console.log(this);
-    var containerDiv = this.parentElement;
-    var container = containerDiv.parentElement
-    
-    containerDiv.classList.add("hidden");
-    container.classList.add("hidden");
+const modalLogin = document.getElementById("modal-login");
+const modalRegister = document.getElementById("modal-register");
+
+for (let button of listButtonCloseModal) {
+  button.addEventListener("click", function () {
+    let containerDiv = this.parentElement;
+    let container = containerDiv.parentElement;
+
+    container.classList.remove("display-flex");
+    container.classList.add("display-hidden");
   });
 }
 
+for (let button of listButtonLoginAccount) {
+  button.addEventListener("click", function () {
+    modalLogin.classList.remove("display-hidden");
+    modalLogin.classList.remove("display-hidden");
+    modalRegister.classList.add("display-hidden");
+  });
+}
 
+for (let button of listButtonCreateAccount) {
+  button.addEventListener("click", function () {
+    modalRegister.classList.remove("display-hidden");
+    modalRegister.classList.remove("display-hidden");
+    modalLogin.classList.add("display-hidden");
+  });
+}
